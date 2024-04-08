ed0dfa6fabac0aec91ff79732053c86d16ffad05
ian.odintsov <odintsov@nutson.us>
2024-04-09 03:14:40 +0400

    Move replace envs in rclone config to start.sh

--- etc/backup/run.sh
+++ etc/backup/run.sh
@@ -1,6 +1,8 @@
 ln -fs "/usr/share/zoneinfo/${BACKUP_TIMEZONE}" /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
 
+envsubst < /userdata/rclone.template.conf > /root/.config/rclone/rclone.conf
+
 envsubst < /userdata/crontask.template > /etc/cron.d/backup
 crontab /etc/cron.d/backup 
 chmod 0644 /etc/cron.d/backup
