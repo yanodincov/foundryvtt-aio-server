3003819e43d6fa7b42a4aff3ccab8b225de15954
ian.odintsov <odintsov@nutson.us>
2024-06-28 05:00:24 +0400

    Fix chmod backup.sh

--- etc/backup/run.sh
+++ etc/backup/run.sh
@@ -6,7 +6,7 @@ envsubst < /userdata/rclone.template.conf > /root/.config/rclone/rclone.conf
 envsubst < /userdata/crontask.template > /etc/cron.d/backup
 crontab /etc/cron.d/backup 
 chmod 0644 /etc/cron.d/backup
-chmod +x /etc/backup/backup.sh
+chmod 1755 /userdata/backup.sh
 cron
 
 tail -f /var/log/cron.log
