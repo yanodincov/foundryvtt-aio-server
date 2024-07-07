5339e5a93c1699c7d0a4bfea93bcb33d6213580f
ian.odintsov <odintsov@nutson.us>
2024-07-08 02:22:40 +0400

    Fix run.sh

--- etc/backup/run.sh
+++ etc/backup/run.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
 ln -fs "/usr/share/zoneinfo/${BACKUP_TIMEZONE}" /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
 
-rclone_ini_config=$(echo -e "[backupstorage]\n$(echo "$json_str" | jq -r 'to_entries[] | "\(.key) = \(.value | tostring)"'))
-echo "Created rclon config from json:\n$rclone_ini_config";
-echo "$rclone_ini_config" > /root/.config/rclone/rclone.conf
+rclone_ini_config=$(echo "$json_str" | jq -r 'to_entries[] | "\(.key) = \(.value | tostring)"')
+echo "Rclone config:\n$rclone_ini_config"
+echo "[backupstorage]\n$rclone_ini_config" > /root/.config/rclone/rclone.conf
 
 envsubst < /userdata/crontask.template > /etc/cron.d/backup
-crontab /etc/cron.d/backup 
 chmod 0644 /etc/cron.d/backup
-cron
+crontab /etc/cron.d/backup
+service cron start
 
 tail -f /var/log/cron.log
