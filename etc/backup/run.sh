92c1083ea13d953f6f6353264751e5bf3618fb57
ian.odintsov <odintsov@nutson.us>
2024-07-08 02:14:15 +0400

    Fix rclone ini config create

--- etc/backup/run.sh
+++ etc/backup/run.sh
@@ -1,10 +1,9 @@
 ln -fs "/usr/share/zoneinfo/${BACKUP_TIMEZONE}" /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
 
-rclone_config=$(jq -r 'to_entries | map("[\(.key)]\n\(.value | to_entries | map("\(.key) = \(.value | tostring)") | .[] ) | .[]' ${RCLONE_JSON_CONFIG})
-# Вывод на экран (можно закомментировать, если не нужен)
-echo "Created rclon config from json:\n$rclone_config";
-echo "$rclone_config" > /root/.config/rclone/rclone.conf
+rclone_ini_config=$(echo -e "[backupstorage]\n$(echo "$json_str" | jq -r 'to_entries[] | "\(.key) = \(.value | tostring)"'))
+echo "Created rclon config from json:\n$rclone_ini_config";
+echo "$rclone_ini_config" > /root/.config/rclone/rclone.conf
 
 envsubst < /userdata/crontask.template > /etc/cron.d/backup
 crontab /etc/cron.d/backup 
